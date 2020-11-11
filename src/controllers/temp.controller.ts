2148c569cca7d3a9d388e275c77d4ad30a4ba225
donghyeKim <arora_@naver.com>
2020-11-11 14:43:01 +0900

    #4_노드익히기

--- src/controllers/temp.controller.ts
+++ src/controllers/temp.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import * as Models from "../utils/db/models";
+import { IAPIError } from "../interfaces/IAPIError";
 
 // 모든 요약 글을 조회하는 함수를 작성해주세요
 export const fetchSummaries = async (request: Request, response: Response) => {
@@ -14,5 +15,15 @@ export const fetchHashtags = async (request: Request, response: Response) => {
     // 데이터 조회는 mongoose를 이용합니다
     // Models.Hashtag를 사용하세요
     // 반드시 response를 통해 응답해야합니다
+    try {
+        const hashtags = await Models.Hashtag.find();
+        response.json(hashtags);
+    } catch (e) {
+        const _error: IAPIError = {
+            displayMessage: "조회 중 오류가 발생했습니다",
+            message: e.message,
+            error: e,
+        };
     response.json("응답");
+    }
 };
